587b7b194292c8d66e3fc39bf719cb60b3ada771
苏锟 <1477264431@qq.com>
2025-03-20 21:53:26 +0800

    去除系统设置选项

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -12,7 +12,6 @@
           <template #dropdown>
             <el-dropdown-menu>
               <el-dropdown-item command="profile">个人信息</el-dropdown-item>
-              <el-dropdown-item command="settings">系统设置</el-dropdown-item>
               <el-dropdown-item divided command="logout">退出登录</el-dropdown-item>
             </el-dropdown-menu>
           </template>
